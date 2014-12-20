6b1aa0fc57fb8cc8fef04b14f552f66dd288c38c
Bibhas <iambibhas@gmail.com>
2014-12-20 11:32:45 +0530

    Added sample home view and template

--- junction/junction/urls.py
+++ junction/junction/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'junction.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^$', TemplateView.as_view(
+        template_name='index.html'
+    ), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
